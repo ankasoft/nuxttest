70bbca8f195c20aeadd7c0ad01adeb95dfd2f4d2
Vehbi Emiroglu <vehbi.emiroglu@gmail.com>
2023-09-12 18:12:39 +0300

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: [
+      'chart.js',
+      'primevue'
+    ]
+  },
+
+  modules: [
+    'nuxt-icon',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@vite-pwa/nuxt',
+    '@vue-macros/nuxt',
+    'nuxt-typed-router',
+    '@nuxtjs/google-fonts',
+    '~/modules/primevue'
+  ],
+
+  css: [
+    'primevue/resources/primevue.css',
+    'primeflex/primeflex.css',
+    'primeicons/primeicons.css',
+    'prismjs/themes/prism-coy.css',
+    '~/assets/styles/layout.scss',
+    '~/assets/demo/flags/flags.css'
+  ],
+
+  dir: {
+    public: 'public/'
+  },
+
+  googleFonts: {
+    families: {
+      Inter: true
+    }
+  },
+
+  imports: {
+    autoImport: true,
+    addons: {
+      vueTemplate: true
+    }
+  },
+
+})
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+ <div>
+   <h1>Index Page</h1>
+   <NuxtLink to="about">
+      <TButton>About</TButton>
+    </NuxtLink>
+ </div>
+</template>
